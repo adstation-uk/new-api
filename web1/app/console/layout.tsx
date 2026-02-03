232b4bfefa8e3d6defcc6e66e694887a0f2487ff
AIchanKimi <1944467655@qq.com>
2026-02-03 16:42:02 +0800

    fix: 修复布局中的主内容区域最大宽度设置

--- web1/app/console/layout.tsx
+++ web1/app/console/layout.tsx
@@ -27,7 +27,7 @@ export default async function ConsoleLayout({
       {/* Secondary Menu */}
       <ConsoleNavbar user={user} />
 
-      <main className="container mx-auto px-4 md:px-8 py-8 max-w-7xl animate-in fade-in duration-500">
+      <main className="container mx-auto px-4 md:px-8 py-8  animate-in fade-in duration-500">
         {children}
       </main>
     </div>
