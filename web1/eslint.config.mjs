83e295539ce17f19a644403453ce54367998f4b6
AIchanKimi <1944467655@qq.com>
2026-02-03 18:06:52 +0800

    feat: 增加eslint

--- web1/eslint.config.mjs
+++ web1/eslint.config.mjs
@@ -1,18 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from '@antfu/eslint-config'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default antfu({
+  react: true,
+  next: true,
+  formatters: true,
+  typescript: true,
+  rules: {
+    'react-refresh/only-export-components': 'off',
+    'ts/consistent-type-definitions': ['error', 'type'],
+  },
+  ignores: [
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'components/ui/**',
+  ],
+})
